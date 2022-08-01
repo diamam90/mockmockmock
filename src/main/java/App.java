6d152a8a10bb6be462a379f89cf7bbaba23dcd41
unknown <diamam90@gmail.com>
2022-08-01 22:25:22 +0300

    убрал таргет, изменил ссылку на папку со стабами

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,13 @@
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+
 
 public class App {
     public static void main(String[] args) {
-        WireMockServer server = new WireMockServer(8000);
+        WireMockServer server = new WireMockServer(WireMockConfiguration.options().port(8000).usingFilesUnderClasspath("src/main/resources/"));
         server.start();
         WireMock.configureFor(8000);
         StubMapping usersStub = WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/users"))
@@ -15,8 +18,11 @@ public class App {
                 .willReturn(WireMock.aResponse()
                         .withBody("Ivan ivanov")
                         .withStatus(200)));
-        server.addStubMapping(usersStub);
-        server.addStubMapping(getUserStub);
-
+        StubMapping soapReq=WireMock.stubFor(WireMock.get(WireMock.urlMatching("/soap/test"))
+                .withHeader("Content-Type",WireMock.containing("xml"))
+                .withRequestBody(WireMock.equalToXml("<header>body</header>")).willReturn(WireMock.okXml("<body>success</body>")));
+        StubMapping soapFromFile = WireMock.stubFor(WireMock.get(WireMock.urlMatching("/soap/file"))
+                .withHeader("Content-Type",WireMock.containing("xml"))
+                .withRequestBody(WireMock.equalToXml("<header>body</header>")).willReturn(WireMock.ok().withBodyFile("test.xml")));
     }
 }
